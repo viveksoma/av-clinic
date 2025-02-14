73a60495ecbfc4a76f37892fd0334dbab1053412
Vivek Somasundaram <viveksomasundaram@Viveks-MacBook-Air.local>
2025-02-14 19:22:31 +0530

    Added main header

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,5 +11,5 @@ $routes->get('/', 'Main\Home::index'); // Map '/' to Main/Home
 $routes->get('/about', 'Main\About::index'); // Map '/' to Main/About
 $routes->get('/service', 'Main\Service::index'); // Map '/' to Main/Service
 $routes->get('/appointment', 'Main\Appointment::index'); // Map '/' to Main/Appointment
-$routes->get('/contact-us', 'Main\Contact::index'); // Map '/' to Main/Appointment
+$routes->get('/contact', 'Main\Contact::index'); // Map '/' to Main/Appointment
 
